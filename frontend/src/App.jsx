a2d4429a827bab5a1966532c247728eb7a47a648
Robin <robingonzales.rg69@gmail.com>
2025-05-07 06:36:09 +0800

    same

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
           path="/pol-dashboard"
           element={
             <AdminRoute>
-              <AdminDashboard />
+              <POLDashboard />
             </AdminRoute>
           }
         />
