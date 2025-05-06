984a16f7b4aadf723db7927c1e0b8e569bb43a24
Robin <robingonzales.rg69@gmail.com>
2025-05-06 09:32:15 +0800

    fix POLLogin design

--- frontend/src/pages/POLLogin.jsx
+++ frontend/src/pages/POLLogin.jsx
@@ -10,6 +10,8 @@ import {
   useToast,
   Text,
   Image,
+  Flex,
+  Container,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -60,66 +62,82 @@ const POLLogin = () => {
   return (
     <Box minH="100vh" bg="gray.100">
       <Navbar showContactButton={false} />
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        p={4}
-        mt={20}
-      >
-        <Box
-          bg="white"
-          p={8}
-          borderRadius="lg"
-          boxShadow="lg"
-          w="100%"
-          maxW="400px"
+      <Container maxW="container.xl" py={10}>
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          minH="calc(100vh - 100px)"
         >
-          <VStack spacing={6} align="stretch">
-            <Image
-              src="/pol.jpg"
-              alt="POL Dump Logo"
-              w="200px"
-              mx="auto"
-              mb={4}
-            />
-            <Heading textAlign="center" size="lg">
-              POL Dump Login
-            </Heading>
-            <form onSubmit={handleSubmit}>
-              <VStack spacing={4}>
-                <FormControl isRequired>
-                  <FormLabel>Username</FormLabel>
-                  <Input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Enter your username"
-                  />
-                </FormControl>
-                <FormControl isRequired>
-                  <FormLabel>Password</FormLabel>
-                  <Input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
-                  />
-                </FormControl>
-                <Button
-                  type="submit"
-                  colorScheme="blue"
-                  w="100%"
-                  isLoading={loading}
-                  loadingText="Logging in..."
-                >
-                  Login
-                </Button>
-              </VStack>
-            </form>
-          </VStack>
-        </Box>
-      </Box>
+          <Box
+            bg="white"
+            p={8}
+            borderRadius="lg"
+            boxShadow="xl"
+            w="100%"
+            maxW="400px"
+            border="1px solid"
+            borderColor="gray.200"
+          >
+            <VStack spacing={6} align="stretch">
+              <Image
+                src="/pol.jpg"
+                alt="POL Dump Logo"
+                w="200px"
+                h="auto"
+                mx="auto"
+                mb={4}
+                borderRadius="md"
+              />
+              <Heading 
+                textAlign="center" 
+                size="lg"
+                color="blue.600"
+                fontWeight="bold"
+              >
+                POL Dump Login
+              </Heading>
+              <form onSubmit={handleSubmit}>
+                <VStack spacing={4}>
+                  <FormControl isRequired>
+                    <FormLabel fontWeight="medium">Username</FormLabel>
+                    <Input
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="Enter your username"
+                      size="lg"
+                      focusBorderColor="blue.500"
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel fontWeight="medium">Password</FormLabel>
+                    <Input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Enter your password"
+                      size="lg"
+                      focusBorderColor="blue.500"
+                    />
+                  </FormControl>
+                  <Button
+                    type="submit"
+                    colorScheme="blue"
+                    w="100%"
+                    size="lg"
+                    isLoading={loading}
+                    loadingText="Logging in..."
+                    mt={4}
+                  >
+                    Login
+                  </Button>
+                </VStack>
+              </form>
+            </VStack>
+          </Box>
+        </Flex>
+      </Container>
     </Box>
   );
 };
