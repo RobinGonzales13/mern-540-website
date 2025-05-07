9506d8ac1077a0fd8e2c7b57befbc8f3a890c6d0
Robin <robingonzales.rg69@gmail.com>
2025-05-07 09:05:51 +0800

    fixing login

--- backend/routes/polAuth.route.js
+++ backend/routes/polAuth.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { login, verifyToken } from "../controllers/polAuth.controller.js";
 
+const router = express.Router();
+
 
 import { createUser } from "../controllers/polAuth.controller.js";
 router.post("/create-user", createUser); // 👈 Add this line
 
-
-const router = express.Router();
 
 router.post("/login", login);
 router.get("/verify", verifyToken);
