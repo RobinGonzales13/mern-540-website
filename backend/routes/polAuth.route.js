28a683673d8fa411c9be7adc9d501233cff4b1b8
Robin <robingonzales.rg69@gmail.com>
2025-05-07 08:54:39 +0800

    testing new user

--- backend/routes/polAuth.route.js
+++ backend/routes/polAuth.route.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { login, verifyToken } from "../controllers/polAuth.controller.js";
 
+
+import { createUser } from "../controllers/polAuth.controller.js";
+router.post("/create-user", createUser); // 👈 Add this line
+
+
 const router = express.Router();
 
 router.post("/login", login);
